2409bdd888df01021c5674101eae2cdc843bb6b6
Lauren Gray <laurenbethgrayx@gmail.com>
2022-02-02 13:48:21 +0000

    erb

--- app.rb
+++ app.rb
@@ -1,16 +1,6 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/' do 
-  "helloooo!"
-end
-
-get '/secret' do
-  'This is a secret page'
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='https://i.imgur.com/jFaSxym.png'>
-   </div>"
+  erb(:index)
 end
